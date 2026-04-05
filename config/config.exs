af817d2a3eb6ccb8b71b93453bbeb72b2c7096cb
Timothy Stapleton <tim.stapleton@hey.com>
2026-04-05 15:10:40 -0500

    scaffold user auth

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :family_hub, :scopes,
+  user: [
+    default: true,
+    module: FamilyHub.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: FamilyHub.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :family_hub,
   ecto_repos: [FamilyHub.Repo],
   generators: [timestamp_type: :utc_datetime]
